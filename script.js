0326f288e2860b1808eca8d1b841108e2a2b75ad
kocovic34k <miljankocovic34k@gmail.com>
2022-11-30 17:58:36 +0100

    First Committ
    Uploading the game

---
+++ script.js
@@ -0,0 +1,58 @@
+'use strict';
+
+let secretNumber = Math.trunc(Math.random() * 20) + 1;
+
+let score = 20;
+
+let highscore = 0;
+
+const displayMessage = function (message) {
+  document.querySelector('.message').textContent = message;
+};
+
+document.querySelector('.check').addEventListener('click', function () {
+  const guess = Number(document.querySelector('.guess').value);
+
+  // No guess
+  if (!guess) {
+    displayMessage('No guess?!');
+
+    // Correct guess
+  } else if (guess === secretNumber) {
+    displayMessage('Correct number! Woah!');
+    document.querySelector('body').style.backgroundColor = '#60b347';
+    document.querySelector('.number').style.width = '30rem';
+    document.querySelector('.number').textContent = secretNumber;
+
+    if (score > highscore) {
+      highscore = score;
+      document.querySelector('.highscore').textContent = highscore;
+    }
+
+    //  Guess to high or low
+  } else if (guess !== secretNumber) {
+    if (score > 1) {
+      document.querySelector('.message').textContent =
+        guess > secretNumber ? 'Too High' : 'Too Low';
+
+      displayMessage(guess > secretNumber ? 'Too High' : 'Too Low');
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      displayMessage('You lost!');
+      document.querySelector('.score').textContent = 0;
+    }
+  }
+});
+
+// Reset the game
+document.querySelector('.again').addEventListener('click', function () {
+  score = 20;
+  secretNumber = Math.trunc(Math.random() * 20) + 1;
+  displayMessage('Guess the number...');
+  document.querySelector('.score').textContent = score;
+  document.querySelector('.number').textContent = '?';
+  document.querySelector('.guess').value = '';
+  document.querySelector('body').style.backgroundColor = '#222';
+  document.querySelector('.number').style.width = '15rem';
+});
